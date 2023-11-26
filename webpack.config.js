54d1ec86cc16d6f3053615f7132512f22fc6dc80
Меркин Арсен <arsen.merkin@gmail.com>
2023-11-26 19:08:42 +0500

    remark app building

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js')
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  }
 }
